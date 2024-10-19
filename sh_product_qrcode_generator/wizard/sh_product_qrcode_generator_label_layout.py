acf7edd36919e5d1d3c7ea46d889c198a3829be5
TRIVESA <david@ecrindefleur.com>
2024-10-19 02:02:41 +0200

    Update sh_product_qrcode_generator_label_layout.py

--- sh_product_qrcode_generator/wizard/sh_product_qrcode_generator_label_layout.py
+++ sh_product_qrcode_generator/wizard/sh_product_qrcode_generator_label_layout.py
@@ -25,40 +25,31 @@ class ShProductQRCodeGeneratorLabelLayout(models.TransientModel):
     columns = fields.Integer(compute='_compute_dimensions')
 
     @api.depends('print_format')
-    def _compute_dimensions(self):
-        for wizard in self:
-            if 'x' in wizard.print_format:
-                columns, rows = wizard.print_format.split('x')[:2]
-                wizard.columns = int(columns)
-                wizard.rows = int(rows)
-            else:
-                wizard.columns, wizard.rows = 1, 1
-
     def _prepare_report_data(self):
-        if self.custom_quantity <= 0:
-            raise UserError(_('You need to set a positive quantity.'))
+    if self.custom_quantity <= 0:
+        raise UserError(_('You need to set a positive quantity.'))
 
-        # Get layout grid
-        if self.print_format == 'dymo':
-            xml_id = 'sh_product_qrcode_generator.sh_report_product_template_label_dymo_with_qr'
-        elif 'x' in self.print_format:
-            xml_id = 'sh_product_qrcode_generator.sh_report_product_template_label_with_qr'
-        else:
-            xml_id = ''
+    # Get layout grid
+    if self.print_format == 'dymo':
+        xml_id = 'sh_product_qrcode_generator.sh_report_product_template_label_dymo_with_qr'
+    elif 'x' in self.print_format:
+        xml_id = 'sh_product_qrcode_generator.sh_report_product_template_label_with_qr'
+    else:
+        xml_id = ''
 
-        active_model = ''
-        if self.product_tmpl_ids:
-            products = self.product_tmpl_ids.ids
-            active_model = 'product.template'
-        elif self.product_ids:
-            products = self.product_ids.ids
-            active_model = 'product.product'
-        else:
-            raise UserError(
-                _("No product to print, if the product is archived please unarchive it before printing its label."))
+    active_model = ''
+    if self.product_tmpl_ids:
+        products = self.product_tmpl_ids.ids
+        active_model = 'product.template'
+    elif self.product_ids:
+        products = self.product_ids.ids
+        active_model = 'product.product'
+    else:
+        raise UserError(
+            _("No product to print, if the product is archived please unarchive it before printing its label."))
 
-        # Build data to pass to the report
-        data = {
+    # Build data to pass to the report
+    data = {
         'active_model': active_model,
         'quantity_by_product': {p: self.custom_quantity for p in products},
         'layout_wizard': self.id,
