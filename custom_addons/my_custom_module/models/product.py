7a098fdebf69f654b6133924b47e5a81186bc76e
TRIVESA <david@ecrindefleur.com>
2024-10-20 19:42:17 +0200

    Update product.py

--- custom_addons/my_custom_module/models/product.py
+++ custom_addons/my_custom_module/models/product.py
@@ -3,4 +3,4 @@ from odoo import models, fields, api
 class ProductTemplate(models.Model):
        _inherit = 'product.template'
 
-       x_studio_item_code = fields.Char(string='商品编码')
+       x_studio_item_code = fields.Char(string=' Item Code')
