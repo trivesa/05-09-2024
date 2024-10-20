71d18075482ae3b378aa3de8ba5398b7c8e3118c
TRIVESA <david@ecrindefleur.com>
2024-10-20 16:06:46 +0200

    Complete my_custom_module setup with product search view inheritance

---
+++ custom_addons/my_custom_module/models/product.py
@@ -0,0 +1,7 @@
+   from odoo import models, fields, api
+
+   class ProductTemplate(models.Model):
+       _inherit = 'product.template'
+
+       x_studio_item_code = fields.Char(string='商品编码')
+   
